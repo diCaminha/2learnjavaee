15fd2da2419b3b74af3f34b173982e352050fff0
denis <dineresc@gmail.com>
2020-06-26 17:52:02 -0300

    refact method to get cars using json-p

--- src/main/java/br/com/caminha/boundary/CarResource.java
+++ src/main/java/br/com/caminha/boundary/CarResource.java
@@ -4,9 +4,13 @@ import br.com.caminha.entity.Car;
 import br.com.caminha.entity.Specification;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.stream.JsonCollectors;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/cars")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,8 +21,16 @@ public class CarResource {
     CarManufacturer carManufacturer;
 
     @GET
-    public List<Car> getCars() {
-        return carManufacturer.retrieveCars();
+    public JsonArray getCars() {
+        List<Car> cars = carManufacturer.retrieveCars();
+
+        return cars.stream()
+                .map(c -> Json.createObjectBuilder()
+                        .add("id", c.getIdentifier())
+                        .add("color", c.getColor().toString())
+                        .add("engine", c.getEngineType().toString())
+                        .add("anotherAttrb", "another attr value")
+                .build()).collect(JsonCollectors.toJsonArray());
     }
 
     @POST
